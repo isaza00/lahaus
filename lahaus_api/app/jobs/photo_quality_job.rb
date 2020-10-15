6e6fc60d8be314dd96af27ef9ed2178c4e579892
Norman <isaza00@hotmail.com>
2020-10-15 13:47:45 -0500

    fixing tests

--- lahaus_api/app/jobs/photo_quality_job.rb
+++ lahaus_api/app/jobs/photo_quality_job.rb
@@ -4,11 +4,10 @@ class PhotoQualityJob < ApplicationJob
   def perform(photo_id)
     photo = Photo.find(photo_id)
     f = IO.popen("python3 example.py #{photo.url}")
+    #f = IO.popen("python3 image_pro.py #{photo.url}")
     result = f.readlines[0].chomp
     f.close
-    #f = IO.popen("python3 image_pro.py #{photo.url}")
     accepted = result.split(" ")
-    puts accepted
     photo.update_attributes(accepted_lum: accepted[0].downcase,
                             accepted_foc: accepted[1].downcase)
   end
